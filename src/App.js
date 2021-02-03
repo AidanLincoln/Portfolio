ff61e0ab955867377c951152a13f1e5ba9193185
Aidan Lincoln <aidantlincoln@gmail.com>
2021-02-03 02:31:11 -0800

    Planet added, working on textures

--- src/App.js
+++ src/App.js
@@ -1,32 +1,57 @@
 import React from 'react';
 import * as THREE from "three";
 import Particles from 'react-particles-js';
-import { render } from "react-dom";
+// import { render } from "react-dom";
 import './App.css';
 
 export default class App extends React.Component {
-  
+
   componentDidMount(){
+
+    let material;
+    let loader;
+    const getTextures = () => new Promise((resolve, reject)=>{
+
+      function loadDone(x) {
+        console.log("loader successfully completed loading task");
+        resolve(x); // it went ok!
+      
+      }
+      // const manager = new THREE.LoadingManager(()=>resolve(textures));
+      loader = new THREE.TextureLoader();
+      loader.crossOrigin = "Anonymous"
+      // "https://i.postimg.cc/dQXDbxyL/planet-Texture.jpg"
+      // https://i.ibb.co/LP9DLD6/hDqdqi7.png
+      // 'https://i.ibb.co/M8Yk7Kc/wallpapersden-com-k-new-abstract-art-2560x1440.jpg'
+      // 'https://i.ibb.co/6tCsF3t/calvin-and-hobbes-background-hd-2560x1440-211373.jpg'
+      const textureLink = 'https://i.ibb.co/dGLgQhN/cyber-futuristic-city-fantasy-art-4k-da-2560x1440.jpg'
+      loader.load(textureLink, loadDone)
+      
+    });
     
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-    camera.position.z = 2;
+    getTextures().then(loadedTexture => {
+      
+      material = new THREE.MeshBasicMaterial( { map: loadedTexture } )  
+      const scene = new THREE.Scene()
+      const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+      camera.position.z = 15;
 
-    var renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true}); 
-    renderer.setClearColor( 0x000000, 0 );
-    renderer.setSize( window.innerWidth, window.innerHeight ); document.getElementById("planet").appendChild( renderer.domElement );  
+      const renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true}); 
+      renderer.setClearColor( 0x000000, 0 );
+      renderer.setSize( window.innerWidth, window.innerHeight ); document.getElementById("planet").appendChild( renderer.domElement );  
 
-    var geometry = new THREE.SphereGeometry( 0.5, 32, 32 );
-    var material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
+      const geometry = new THREE.SphereGeometry( 5, 32, 32 );
+      
+      const planet = new THREE.Mesh( geometry, material );
+      scene.add( planet );
 
-    var planet = new THREE.Mesh( geometry, material );
-    scene.add( planet );
+      const animate = function () {
+        requestAnimationFrame( animate );
+        renderer.render( scene, camera );
+      }
+      animate();
+    })
 
-    var animate = function () {
-      requestAnimationFrame( animate );
-      renderer.render( scene, camera );
-    }
-    animate();
   }
 
   render(){
@@ -72,7 +97,7 @@ export default class App extends React.Component {
                   }
                 },
                 size: {
-                  value: 3,
+                  value: 4,
                   random: true,
                   anim: {
                     enable: false,
